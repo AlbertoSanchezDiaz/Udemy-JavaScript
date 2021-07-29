b6b4e1c955d1fe4749c7f9a0057b479321094e37
CodeHennessy <albesandi1208@gmail.com>
2021-07-29 13:33:27 +0200

    commit3

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,4 @@
-console.log('Hola Mundo')
+// console.log('Hola Mundo')
 
 // Tipos de datos en JS
 // String: Cadena de caracteres.
@@ -15,7 +15,7 @@ console.log('Hola Mundo')
 // const
 
 let miPrimeraVariable = "Mi primera variable"
-console.log(miPrimeraVariable);
+// console.log(miPrimeraVariable);
 
 let miBoolean = true
 let miOtroBool = false
@@ -24,4 +24,28 @@ let miNumero = 0
 let miNumero2 = 12
 let miNumero3 = -250
 
-console.log(miNumero, miNumero2, miBoolean, miPrimeraVariable);
+// console.log(miNumero, miNumero2, miBoolean, miPrimeraVariable);
+
+const arrVacio = []
+const arr = [1,2,"Hola", miNumero]
+
+// console.log(arrVacio,arr);
+
+arr.push(10)
+// console.log(arr);
+
+arr.push(miPrimeraVariable)
+// console.log(arr);
+
+const suma = 1 + 2
+const resta = 1 - 2
+const multiplicar = 2 * 3
+const division = 9 / 3
+// console.log(suma,resta,multiplicar,division);
+
+const modulo = 10 % 3
+// console.log(modulo);
+
+let num = 5
+num ++
+console.log(num);
