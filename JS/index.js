f7704c481c9c72f43a7cddf81d088626b9b32143
CodeHennessy <albesandi1208@gmail.com>
2021-07-29 12:13:03 +0200

    commit3

---
+++ JS/index.js
@@ -0,0 +1,27 @@
+console.log('Hola Mundo')
+
+// Tipos de datos en JS
+// String: Cadena de caracteres.
+// Boolean: True or false.
+// Null: Nulo.
+// Number: 123123 1 12 123.
+// "123" es un String, distinto a 123 que es un number.
+// Undefined.
+// Object: Objeto que agrupa los distintos datos.
+
+// Definicion de variables
+// var
+// let
+// const
+
+let miPrimeraVariable = "Mi primera variable"
+console.log(miPrimeraVariable);
+
+let miBoolean = true
+let miOtroBool = false
+
+let miNumero = 0
+let miNumero2 = 12
+let miNumero3 = -250
+
+console.log(miNumero, miNumero2, miBoolean, miPrimeraVariable);
