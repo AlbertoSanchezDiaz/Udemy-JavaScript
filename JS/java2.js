38d3a171bf8d7cecc0159a488139f63dd52ff3ab
CodeHennessy <albesandi1208@gmail.com>
2021-08-06 12:29:34 +0200

    commit2

--- JS/java2.js
+++ JS/java2.js
@@ -1,4 +1,14 @@
+const todos = [];
+
 window.onload = () => {
-    const parrafo = document.getElementById('text')
-    parrafo.innerHTML = 'Texto actualizado'
+    const form = document.getElementById("todo-form");
+    form.onsubmit = (e) => {
+        e.preventDefault();
+        const todo = document.getElementById("todo");
+        const todoText = todo.value;
+        todo.value = '';
+        todos.push(todoText);
+        const todoList = document.getElementById("todo-list");
+        const todosTemplate = todos.map(t => )
+    }
 }
